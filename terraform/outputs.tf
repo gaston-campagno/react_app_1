5e77d11ac50e5842b029858550515cf2f8efe046
quasarlights <gastonestebancampagno@gmail.com>
2024-09-29 18:08:41 -0300

    fix ssh key

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,12 +5,14 @@ output "ec2_instance_public_ip" {
 
 # Output de la clave privada para usarla en el workflow
 output "ssh_private_key" {
-  value     = tls_private_key.ssh_key.private_key_pem
+  value     = tls_private_key.ssh_key[0].private_key_pem  # Acceso a la primera instancia
   sensitive = true # Asegura que no se muestre en los logs de Terraform
 }
 
 output "ssh_public_key" {
-  value = tls_private_key.ssh_key.public_key_openssh
+  value = length(data.aws_key_pair.existing_key.key_name) > 0 ? 
+          data.aws_key_pair.existing_key.public_key : 
+          tls_private_key.ssh_key[0].public_key_openssh  # Acceso a la primera instancia
 }
 # Salida que muestra el ID de la instancia
 output "ec2_instance_id" {
