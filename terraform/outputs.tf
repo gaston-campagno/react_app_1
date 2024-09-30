b51454e7ee64e0fe482875963d9b6c729b703c74
quasarlights <gastonestebancampagno@gmail.com>
2024-09-30 00:06:32 -0300

    my-ssh-key from third

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,10 +3,13 @@ output "ec2_instance_public_ip" {
   value = aws_spot_instance_request.k8s_node.public_ip
 }
 
-
 output "ssh_private_key" {
-  value     = tls_private_key.ssh_key[0].private_key_pem
-  sensitive = true # Marca la clave como sensible para ocultarla en la salida
+  value = length(data.aws_key_pair.existing_key.key_name) == 0 ? tls_private_key.ssh_key[0].private_key_pem : "No key created"
+  sensitive = true
+}
+
+output "key_name" {
+  value = aws_key_pair.my_key[0].key_name
 }
 # Salida que muestra el ID de la instancia
 output "ec2_instance_id" {
