dd3b0470778b90dc3b08eeabaab9634cf882308e
quasarlights <gastonestebancampagno@gmail.com>
2024-09-30 16:36:11 -0300

    fix instance id ip

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,9 +1,9 @@
 # Salida que muestra la IP pública de la instancia
 output "ec2_instance_public_ip" {
-  value = aws_spot_instance_request.k8s_node.public_ip
+  value = aws_instance.k8s_node.public_ip
 }
 
 # Salida que muestra el ID de la instancia
 output "ec2_instance_id" {
-  value = aws_spot_instance_request.k8s_node.id
+  value = aws_instance.k8s_node.id
 }
