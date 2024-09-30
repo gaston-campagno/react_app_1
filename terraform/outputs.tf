9e78413afb4a4db4e2f62448361f5cf4611c0f82
quasarlights <gastonestebancampagno@gmail.com>
2024-09-30 01:06:44 -0300

    correct outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,9 +1,7 @@
-# Salida que muestra la IP pública de la instancia
 output "ec2_instance_public_ip" {
-  value = aws_spot_instance_request.k8s_node.public_ip
+  value = aws_spot_instance_request.k8s_node[0].public_ip  # Accede a la primera instancia
 }
 
-# Salida que muestra el ID de la instancia
 output "ec2_instance_id" {
-  value = aws_spot_instance_request.k8s_node.id
+  value = aws_spot_instance_request.k8s_node[0].id  # Accede a la primera instancia
 }
