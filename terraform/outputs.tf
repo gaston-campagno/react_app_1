929292e0e13c04242b06f52b112a634935b799bb
easyminecraftserver <easyminecraftserver@gmail.com>
2024-09-30 01:07:50 -0300

    terraform fmt again

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,7 @@
 output "ec2_instance_public_ip" {
-  value = aws_spot_instance_request.k8s_node[0].public_ip  # Accede a la primera instancia
+  value = aws_spot_instance_request.k8s_node[0].public_ip # Accede a la primera instancia
 }
 
 output "ec2_instance_id" {
-  value = aws_spot_instance_request.k8s_node[0].id  # Accede a la primera instancia
+  value = aws_spot_instance_request.k8s_node[0].id # Accede a la primera instancia
 }
