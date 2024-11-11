213d3bc14b65baf33a8633d35a633ca29be52efb
quasarlights <gastonestebancampagno@gmail.com>
2024-11-11 15:45:03 -0300

    switch ec2 instance type to t2.micro

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,7 +7,7 @@ variable "vpc_id" {
 variable "instance_type" {
   description = "Type of EC2 instance"
   type        = string
-  default     = "t3.medium"
+  default     = "t2.micro"
 }
 
 variable "ami_id" {
